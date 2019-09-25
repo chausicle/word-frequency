a30f30ea0acee782475d1c1834948e82e13564be
Justin Chau <chausicle@gmail.com>
2019-09-24 19:00:02 -0700

    Initial setup

---
+++ app.py
@@ -0,0 +1,21 @@
+def main():
+    print("This is main")
+
+    keyword_dataframe()
+    generate_wordcloud()
+
+
+def keyword_dataframe():
+    print("This is the keyword dataframe")
+
+    weightage()
+
+
+def weightage():
+    print("This is the weightage")
+
+
+def generate_wordcloud():
+    print("Generate wordcloud")
+
+main()
