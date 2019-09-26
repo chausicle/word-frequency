5dcc857c429bde6fe14fa7559ede176e7c690090
Justin Chau <chausicle@gmail.com>
2019-09-26 03:19:03 -0700

    Generate multiform frequency and wordcloud

--- app.py
+++ app.py
@@ -36,6 +36,12 @@ def main():
 
     # checks for peer-reference-forms folder in Desktop to analyze
     if 'peer-reference-forms' in os.listdir(desktop_path):
+        # warn user if there's no files in peer-reference-forms folder
+        if len(os.listdir(os.path.join(desktop_path, 'peer-reference-forms'))) == 0:
+            print('Please put in at least one form into "peer-reference-forms" for analysis')
+            print(
+                'Run code again once a folder named peer-reference-forms is located in Desktop with forms to analyze.')
+            return None
         # create a output folder if none is found on Desktop
         if 'word-frequency-output' not in os.listdir(desktop_path):
             os.makedirs(os.path.join(desktop_path, 'word-frequency-output'))
@@ -84,9 +90,9 @@ def main():
                 keywords, df_keywords, success = keyword_dataframe(
                     path_to_file, filename, path_to_keywords, successful_analysis
                 )
-                # collect keywords for one large word frequency
+                # collect keywords for one large word frequency,
                 # generate individual wordclouds
-                # save individual DataFrames
+                # and save individual DataFrames
                 if keywords != 'Error':
                     multiform_keywords.extend(keywords)
                     successful_analysis = success
@@ -102,11 +108,40 @@ def main():
 
         end_time = datetime.datetime.now()
         analysis_time = end_time - start_time
+
         print('Analysis time start: {}'.format(start_time))
         print('Analysis time end: {}'.format(end_time))
         print('Successfully analyzed {} forms out of {}'.format(
             successful_analysis, file_length)
         )
+        print('Runtime analysis: {} seconds'.format(analysis_time.seconds))
+        print('\nBeginning to process multiform analysis...\n')
+
+        start_time = datetime.datetime.now()
+        path_to_multiform_frequency = os.path.join(
+            desktop_path, 'word-frequency-output',
+            'multiform_analysis', 'multiform_frequencies.csv'
+        )
+        path_to_multiform_wordcloud = os.path.join(
+            desktop_path, 'word-frequency-output',
+            'multiform_analysis', "multiform_wordcloud.pdf"
+        )
+
+        # create multiform frequency and save to output file multiform_analysis
+        keyword_dataframe(
+            '', 'multiform_frequencies',
+            path_to_multiform_frequency, successful_analysis,
+            True, multiform_keywords
+        )
+
+        # create multiform wordcloud and save to output file multiform_analysis
+        generate_wordcloud(multiform_keywords, path_to_multiform_wordcloud)
+
+        end_time = datetime.datetime.now()
+        analysis_time = end_time - start_time
+
+        print('Analysis time start: {}'.format(start_time))
+        print('Analysis time end: {}'.format(end_time))
         print('Runtime analysis: {} seconds'.format(analysis_time.seconds))
     # prints out warning when peer-reference-forms folder isn't located in Desktop
     else:
@@ -114,7 +149,11 @@ def main():
         print('Run code again once a folder named peer-reference-forms is located in Desktop with forms to analyze.')
 
 
-def keyword_dataframe(path_to_file, filename, path_to_keywords, successful_analysis):
+def keyword_dataframe(
+        path_to_file, filename,
+        path_to_keywords, successful_analysis,
+        is_multi=False, multiform_keywords=[],
+        number_of_documents=1):
     """
     Generate word frequency DataFrame from text extracted off of document.
 
@@ -122,36 +161,52 @@ def keyword_dataframe(path_to_file, filename, path_to_keywords, successful_analy
     :param filename: Name of file
     :param path_to_keywords: Path to keyword frequency folder
     :param successful_analysis: Number of successful analysis
+    :param is_multi: Boolean to change process of analysis
+    :param multiform_keywords: List of words from all successful analyzed documents
+    :param number_of_documents: Integer for number of documents
     :return keywords: List of all keywords
     :return df_keywords: DataFrame of word frequencies
     :return successful_analysis: Counter for successful analyses
     """
     try:
         stopwords = set(STOPWORDS)
-
-        text = textract.process(path_to_file, method='tesseract',
+        # only extracts text from document if it's a single document
+        if not is_multi:
+            text = textract.process(path_to_file, method='tesseract',
                                 language='eng', encoding='ascii'
                                 ).decode('utf-8')
 
-        if text == '':
-            return 'Error', 'Error', 'Error'
+            # returns 3 Error strings just so code doesn't break
+            # since the return is expected to unpack 3 variables
+            if text == '':
+                return 'Error', 'Error', 'Error'
 
-        text = text.lower()
-        keywords = re.findall(r'[a-zA-Z]\w+', text)
+            text = text.lower()
+            keywords = re.findall(r'[a-zA-Z]\w+', text)
+        else:
+            number_of_documents = successful_analysis
+            keywords = multiform_keywords
 
         df = pd.DataFrame(list(set(keywords)), columns=["keywords"])
 
-        df['number_of_times_word_appeared'] = df['keywords'].apply(lambda word: weightage(word, keywords)[0])
-        df['tf'] = df['keywords'].apply(lambda word: weightage(word, keywords)[1])
-        df['idf'] = df['keywords'].apply(lambda word: weightage(word, keywords)[2])
-        df['tf_idf'] = df['keywords'].apply(lambda word: weightage(word, keywords)[3])
+        df['number_of_times_word_appeared'] = df['keywords']\
+            .apply(lambda word: weightage(word, keywords, number_of_documents)[0])
+        df['tf'] = df['keywords']\
+            .apply(lambda word: weightage(word, keywords, number_of_documents)[1])
+        df['idf'] = df['keywords']\
+            .apply(lambda word: weightage(word, keywords, number_of_documents)[2])
+        df['tf_idf'] = df['keywords']\
+            .apply(lambda word: weightage(word, keywords, number_of_documents)[3])
         df['timestamp'] = datetime.datetime.now().isoformat()
 
         df = df.sort_values('tf_idf', ascending=True)
         df_keywords = df[~df['keywords'].isin(list(stopwords))]
         df_keywords.to_csv(path_to_keywords)
         successful_analysis += 1
-        print('DataFrame creation on file "{}" successful'.format(filename))
+        if not is_multi:
+            print('DataFrame creation on file "{}" successful'.format(filename))
+        else:
+            print('DataFrame creation on all successful document analysis')
 
         return keywords, df_keywords, successful_analysis
     except Exception as e:
@@ -191,6 +246,7 @@ def generate_wordcloud(keywords, path_to_wordcloud, filename):
 
     :param keywords: String of keywords found from text
     :param path_to_wordcloud: String path to wordcloud folder
+    :param filename: String of filename for printing on Terminal
     """
     word_cloud = WordCloud(width=800, height=800,
                            background_color='white',
@@ -202,7 +258,9 @@ def generate_wordcloud(keywords, path_to_wordcloud, filename):
     plt.axis('off')
     plt.tight_layout(pad=0)
     plt.savefig(path_to_wordcloud)
-    print('Wordcloud creation on file "{}" successful'.format(filename))
-
+    if not filename:
+        print('Wordcloud creation on file "{}" successful'.format(filename))
+    else:
+        print('Worldcloud creation on all successful document analysis')
 
 main()
